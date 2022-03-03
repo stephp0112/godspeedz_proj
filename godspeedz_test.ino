a6fe8cbb5404a9d8fa5a896385dec242207621af
stephp0112 <100496220+stephp0112@users.noreply.github.com>
2022-03-03 14:24:27 -0600

    updated test code

--- godspeedz_test.ino
+++ godspeedz_test.ino
@@ -1,33 +1,33 @@
 int ledPin = 8;        //LED light pin
-int sensor = 2;         //sensor pin
-int state = LOW;       //no motion is detected
-int val = 0;           //variable that stores the sensor status
+int motionSensor = 2;         //motionSensor pin
+int sensorVal = LOW;       //no motion is detected
+//int sensorVal = 0;           //variable that stores the motionSensor status
 
 
 void setup() {
   pinMode(ledPin, OUTPUT);       //assigning the LED as an output
-  pinMode(sensor, INPUT);    //assigning the PIR as the input
+  pinMode(motionSensor, INPUT);    //assigning the PIR as the input
   Serial.begin(9600);       
 }
 
 void loop() {
-  val = digitalRead(sensor);    
-  if (val == HIGH) {          //if sensor is high,
+  sensorVal = digitalRead(motionSensor);    
+  if (sensorVal == HIGH) {          //if motionSensor is high,
     digitalWrite(ledPin, HIGH); //the LED will turn on
-    delay(500);            
+//    delay(100);            
 
-    if (state == LOW) {
+    if (sensorVal == LOW) {
       Serial.println("Groceries have been detected!");
-      state = HIGH;         //updating the variable state to HIGH
+      sensorVal = HIGH;         //updating the variable state to HIGH
     }
   }
   else {
     digitalWrite(ledPin, LOW);   //LED turns off
-    delay(500);
+//    delay(100);
 
-    if (state == HIGH) {
+    if (sensorVal == HIGH) {
       Serial.println("Groceries have been moved");  //updating variable state to LOW
-      state = LOW;
+      sensorVal = LOW;
     }
   }
 }
